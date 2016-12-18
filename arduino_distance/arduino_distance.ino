fae7f4f094818f03d2bece2d36fb9bd4b0de0dd8
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2016-12-18 20:55:38 +0000

    Huge changes to distance measuring and averages

--- arduino_distance/arduino_distance.ino
+++ arduino_distance/arduino_distance.ino
@@ -1,8 +1,8 @@
-/* This example shows how to use continuous mode to take
-range measurements with the VL53L0X. It is based on
-vl53l0x_ContinuousRanging_Example.c from the VL53L0X API.
-
-The range readings are in units of mm. */
+/* arduino_distance
+ * Uses VL53L0X Time-of-Flight Lidar sensor for distance measurement
+ * Stepper motor rotation
+ * Work in progress - Matthew Timmons-Brown
+ */
 
 #include <Wire.h>
 #include <VL53L0X.h>
@@ -13,22 +13,44 @@ void setup()
 {
   Serial.begin(9600);
   Wire.begin();
-
   sensor.init();
   sensor.setTimeout(500);
-
-  // Start continuous back-to-back mode (take readings as
-  // fast as possible).  To use continuous timed mode
-  // instead, provide a desired inter-measurement period in
-  // ms (e.g. sensor.startContinuous(100)).
-  sensor.startContinuous();
+  sensor.setMeasurementTimingBudget(100000); // For higher accuracy measurement (up to 100ms)(value in microseconds)
 }
 
 void loop()
 {
-  Serial.print(sensor.readRangeContinuousMillimeters());
-  delay(200);
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-
+  int a;
+  int lidar_array[3];
+  populate_lidar_array(lidar_array);
+  a = average_lidar_array(lidar_array);
+  Serial.print(a);
   Serial.println();
 }
+
+int populate_lidar_array(int lidar_array[])
+{
+  int i;
+  int len = sizeof(lidar_array);
+  for (i = 0; i = (len-1); i = i +1)
+  {
+    lidar_array[i] = sensor.readRangeSingleMillimeters();
+  }
+}
+
+int average_lidar_array(int lidar_array[])
+{
+  int i;
+  int len = sizeof(lidar_array);
+  int sum = 0;
+  for (i = 0; i = (len-1); i = i + 1)
+  {
+    sum += lidar_array[i];  
+  }
+  int result;
+  result = ((float) sum) / len;
+  return result;
+}
+  
+  
+
