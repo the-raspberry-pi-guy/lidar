fd6226bbdcfc4cacbbf7d7a57869afe691440243
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2016-12-18 23:10:28 +0000

    Add comments

--- arduino_distance/arduino_distance.ino
+++ arduino_distance/arduino_distance.ino
@@ -1,15 +1,20 @@
 /* arduino_distance
- * Uses VL53L0X Time-of-Flight Lidar sensor for distance measurement
- * Stepper motor rotation
+ * Uses VL53L0X Time-of-Flight Lidar sensor for distance measurement. Hand crafted averaging for greater precision
+ * Stepper motor rotation. In progress.
  * Work in progress - Matthew Timmons-Brown
  */
 
+// Import I2C and sensor library
 #include <Wire.h>
 #include <VL53L0X.h>
+
+// Set a constant - number of trials before averaging
 #define average_trials 3
 
+// Initialise sensor
 VL53L0X sensor;
 
+// Setup loop
 void setup()
 {
   Serial.begin(9600);
@@ -21,10 +26,11 @@ void setup()
 
 void loop()
 {
+  // Initiliase variables
   int avg;
-  int lidar_array[average_trials];
-  populate_lidar_array(lidar_array);
-  avg = average_lidar_array(lidar_array);
+  int lidar_array[average_trials]; // Create array
+  populate_lidar_array(lidar_array); // Pass array into populate_lidar_array function and fill with distance values
+  avg = average_lidar_array(lidar_array); // Average array values and store in avg variable
   Serial.print(avg);
   Serial.println();
 }
@@ -33,6 +39,7 @@ int populate_lidar_array(int lidar_array[])
 {
   for (int i = 0; i < average_trials; i = i +1)
   {
+    // For each index of the array, get distance and store distance at that index
     lidar_array[i] = get_distance();
   }
 }
@@ -42,15 +49,17 @@ int average_lidar_array(int lidar_array[])
   int sum = 0;
   for (int i = 0; i < average_trials; i = i + 1)
   {
+    // Sum each value in the array
     sum += lidar_array[i];  
   }
-  int result;
+  int result; // Work out mean average and return result
   result = ((float) sum) / average_trials;
   return result;
 }
   
 int get_distance()
 {
+  // Get distance in millimeters from lidar sensor
   return sensor.readRangeSingleMillimeters();
 }
 
