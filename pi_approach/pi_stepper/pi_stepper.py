cb39ee38cda84317f859d6474d246bd9fa10dc39
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2016-12-20 09:17:28 +0000

    Flesh out stepper script with exact logic

--- pi_approach/pi_stepper/pi_stepper.py
+++ pi_approach/pi_stepper/pi_stepper.py
@@ -11,15 +11,17 @@ class stepper_controller(object):
 	""" An all-powerful stepper motor controller"""
 	
 	current_step = -1
-	motor_sequence = [[1.0, 1.0], [1.0, -1.0], [-1.0, -1.0], [-1.0, 1.0]] # Order for stepping
+	power = 0.5 # Power to coils
+	motor_sequence = [[power, power], [power, -power], [-power, -power], [-power, power]] # Order for stepping
 	step_delay = 0.002                                               # Delay between steps
-
+	
+	progress = 0
 
 	def __init__(self):
 		motor.Init()
 		motor.ResetEpo()
 
-	def move_steps(self,count):
+	def move_steps_transceive(self,count):
 		# Reverse handling
 		if count < 0:
 			dir = -1
@@ -27,7 +29,8 @@ class stepper_controller(object):
 		else:
 			dir = 1
 		
-		while count > 0:
+			
+		while (count > 0):
 			# If this is the first move, set starting position
 		        if stepper_controller.current_step == -1:
             			drive = stepper_controller.motor_sequence[-1] # Access list from right
@@ -49,17 +52,33 @@ class stepper_controller(object):
             			motor.SetMotor1(drive[0])
             			motor.SetMotor2(drive[1])
         		
+			stepper_controller.progress += 1
+			degrees = progress*1.8
+
+			# TRANSMIT POSITION DATA
+			transmit(SOME PARAMETERS)
+
 			time.sleep(stepper_controller.step_delay)
         		count -= 1
+		
+		motor.MotorsOff()
 
+	def transmit(self):
+		# TRANSMIT PARAMETERS
+	
 	def main(self):
-		try:
-			motor.MotorsOff()
-			while True:
-				steps = input("Steps to move: ")
-				stepper_controller.move_steps(self, steps)
-		except KeyboardInterrupt:
-			motor.MotorsOff()
+		motor.MotorsOff()
+		while True:
+			if # receive #
+				if # single rotation #
+					steps = 200
+					stepper_controller.move_steps_transceive(self, steps)
+				if # cont rotation #
+					cont = true
+					while cont = true:
+						stepper_controller.move_steps_transceive(self, 1)
+						if # stop command #
+							cont = false
 
 stepp = stepper_controller()
 stepp.main()
