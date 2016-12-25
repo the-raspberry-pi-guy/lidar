7d7b966a3378d345c66da413946dc8f678b74a25
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2016-12-25 19:28:57 +0000

    Distance in python updates

--- pi_approach/Distance_Pi/distance_library.py
+++ pi_approach/Distance_Pi/distance_library.py
@@ -4,7 +4,7 @@ import smbus
 import sys
 import time
 
-class distance_sensor(object):
+class distance(object):
 	""" An all-powerful distance sensor controller"""
 
 	VL53L0X_REG_IDENTIFICATION_MODEL_ID             = 0x00c0
@@ -23,26 +23,70 @@ class distance_sensor(object):
 	def bswap(self, val):
     		return struct.unpack('<H', struct.pack('>H', val))[0]
 	def mread_word_data(self, adr, reg):
-    		return bswap(bus.read_word_data(adr, reg))
+    		return bswap(distance.bus.read_word_data(adr, reg))
 	def mwrite_word_data(self, adr, reg, data):
-    		return bus.write_word_data(adr, reg, bswap(data))
+    		return distance.bus.write_word_data(adr, reg, distance.bswap(data))
 	def makeuint16(self, lsb, msb):
     		return ((msb & 0xFF) << 8)  | (lsb & 0xFF)
-	def VL53L0X_decode_vcsel_period(self,vcsel_period_reg):
+	
+	@staticmethod	
+	def VL53L0X_decode_vcsel_period(vcsel_period_reg):
 		# Converts the encoded VCSEL period register value into the real
 		# period in PLL clocks
 		vcsel_period_pclks = (vcsel_period_reg + 1) << 1;
 		return vcsel_period_pclks;
 
 	def get_revision_ID(self):
-		ID = bus.read_byte_data(address, distance_sensor.VL53L0X_REG_IDENTIFICATION_REVISION_ID)
+		ID = distance.bus.read_byte_data(distance.address, distance.VL53L0X_REG_IDENTIFICATION_REVISION_ID)
 		return hex(ID)
 	
 	def get_device_ID(self):
-		ID = bus.read_byte_data(address, distance_sensor.VL53L0X_REG_IDENTIFICATION_MODEL_ID)
+		ID = distance.bus.read_byte_data(distance.address, distance.VL53L0X_REG_IDENTIFICATION_MODEL_ID)
 		return hex(ID)
 
 	def get_prerange_config_vcsel_period(self):
-		period = bus.read_byte_data(address, distance_sensor.VL53L0X_REG_PRE_RANGE_CONFIG_VCSEL_PERIOD)
-		output = "Final range config vcsel period: " + hex(period) + " decode: " + str(distance_sensor.VL53L0X_decode_vcsel_period(period)
+		period = distance.bus.read_byte_data(distance.address, distance.VL53L0X_REG_PRE_RANGE_CONFIG_VCSEL_PERIOD)
+		output = "Pre range config vcsel period: " + str(hex(period)) + " decode: " + str(distance.VL53L0X_decode_vcsel_period(period))
 		return output
+	
+	def get_finalrange_config_vcsel_period(self):
+		period = distance.bus.read_byte_data(distance.address, distance.VL53L0X_REG_FINAL_RANGE_CONFIG_VCSEL_PERIOD)
+		output = "Final range config vcsel period: " + str(hex(period)) + " decode: " + str(distance.VL53L0X_decode_vcsel_period(period))
+		return output
+	
+	def set_sysrange_start(self):
+		sysrange = distance.bus.write_byte_data(distance.address, distance.VL53L0X_REG_SYSRANGE_START, 0x01)	
+		return "done sysrange"
+
+	def get_ready_status(self):
+		cnt = 0
+		while (cnt < 100):
+			time.sleep(0.10)
+			range_status = distance.bus.read_byte_data(distance.address, distance.VL53L0X_REG_RESULT_RANGE_STATUS)
+			if (range_status & 0x01):
+				break
+			cnt += 1
+		if (range_status & 0x01):
+			return "ready"
+		else:
+			return "not ready"
+
+	def get_distance(self):
+		data = distance.bus.read_i2c_block_data(distance.address, 0x14, 12)
+		output = int(distance.makeuint16(self, data[11], data[10]))
+		return output
+
+d = distance()
+print d.get_revision_ID()
+print d.get_device_ID()
+print d.get_prerange_config_vcsel_period()
+print d.get_finalrange_config_vcsel_period()
+print d.get_ready_status()
+while True:
+#	time.sleep(0.2)
+	d.set_sysrange_start()
+	val = d.get_distance()
+	if val == 20:
+		print "Failed"
+	else:
+		print val
