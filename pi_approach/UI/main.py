a37f9cbdab7a00b3687f2f1161b9cd89fea847a6
Matt <theraspberrypiguy@gmail.com>
2017-01-20 10:02:37 +0000

    Continue work on the kivy lay out of the main app

--- pi_approach/UI/main.py
+++ pi_approach/UI/main.py
@@ -2,16 +2,20 @@
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 
-Window.clearcolor=(1,1,1,1)
+#Window.clearcolor=(1,1,1,1)
 
 class Init_Screen(GridLayout):
-    pass
+	pass
+
+class Main_Screen(FloatLayout):
+	pass
 
 class LidarApp(App):
-    def build(self):
-        return Init_Screen()
+	def build(self):
+		return Main_Screen()
 
 if __name__ == '__main__':
-    LidarApp().run()
+	LidarApp().run()
