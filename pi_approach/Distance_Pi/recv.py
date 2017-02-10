361c0293085a688cc60e06b675eca66c9c52d72e
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2017-02-10 08:35:16 +0000

    Remove local use of client, instead rely on imported library

--- pi_approach/Distance_Pi/recv.py
+++ pi_approach/Distance_Pi/recv.py
@@ -3,28 +3,15 @@
 import serial
 import socket
 import time
+import sys
+sys.path.insert(0, "/home/pi/lidar/pi_approach/Libraries")
+import serverxclient
 
 arduino_dist = serial.Serial('/dev/ttyUSB0',9600)
 
 def get_distance():
 	distance = arduino_dist.readline()
 	return distance
-
-class Client(object):
-	"""A class that uses sockets to connect to a server"""
-	HOST = "userinterface.local"
-	PORT = 12345
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	
-	def socket_connection(self):
-		Client.s.connect((Client.HOST, Client.PORT))
-	
-	def receive_data(self):
-		data = Client.s.recv(4096)
-		return data
-	
-	def send_data(self, data):
-		Client.s.send(data)
 
 client = Client()
 client.socket_connection()
