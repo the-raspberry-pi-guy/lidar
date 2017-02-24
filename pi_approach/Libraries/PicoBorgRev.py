8c3b779c68c1172663c70386027318d4e82c5451
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2017-02-24 09:54:04 +0000

    Comment on stepper motor interface controller

--- pi_approach/Libraries/PicoBorgRev.py
+++ pi_approach/Libraries/PicoBorgRev.py
@@ -1,30 +1,7 @@
-#!/usr/bin/env python
-# coding: latin-1
-"""
-This module is designed to communicate with the PicoBorg Reverse
-
-Use by creating an instance of the class, call the Init function, then command as desired, e.g.
-import PicoBorgRev
-PBR = PicoBorgRev.PicoBorgRev()
-PBR.Init()
-# User code here, use PBR to control the board
-
-Multiple boards can be used when configured with different I²C addresses by creating multiple instances, e.g.
-import PicoBorgRev
-PBR1 = PicoBorgRev.PicoBorgRev()
-PBR2 = PicoBorgRev.PicoBorgRev()
-PBR1.i2cAddress = 0x44
-PBR2.i2cAddress = 0x45
-PBR1.Init()
-PBR2.Init()
-# User code here, use PBR1 and PBR2 to control each board separately
-
-For explanations of the functions available call the Help function, e.g.
-import PicoBorgRev
-PBR = PicoBorgRev.PicoBorgRev()
-PBR.Help()
-See the website at www.piborg.org/picoborgreverse for more details
-"""
+# PicoBorgRev.py
+# Library to interface between Raspberry Pi and PicoBorgRev - the stepper motor controller
+# Original Author: Freeburn Robotics Limited
+# Modified by Matthew Timmons-Brown
 
 # Import the libraries we need
 import smbus
@@ -80,11 +57,11 @@ def ScanForPicoBorgReverse(busNumber = 1):
     """
 ScanForPicoBorgReverse([busNumber])
 
-Scans the I²C bus for a PicoBorg Reverse boards and returns a list of all usable addresses
-The busNumber if supplied is which I²C bus to scan, 0 for Rev 1 boards, 1 for Rev 2 boards, if not supplied the default is 1
+Scans the IÂ²C bus for a PicoBorg Reverse boards and returns a list of all usable addresses
+The busNumber if supplied is which IÂ²C bus to scan, 0 for Rev 1 boards, 1 for Rev 2 boards, if not supplied the default is 1
     """
     found = []
-    print 'Scanning I²C bus #%d' % (busNumber)
+    print 'Scanning IÂ²C bus #%d' % (busNumber)
     bus = smbus.SMBus(busNumber)
     for address in range(0x03, 0x78, 1):
         try:
@@ -114,25 +91,25 @@ def SetNewAddress(newAddress, oldAddress = -1, busNumber = 1):
     """
 SetNewAddress(newAddress, [oldAddress], [busNumber])
 
-Scans the I²C bus for the first PicoBorg Reverse and sets it to a new I2C address
+Scans the IÂ²C bus for the first PicoBorg Reverse and sets it to a new I2C address
 If oldAddress is supplied it will change the address of the board at that address rather than scanning the bus
-The busNumber if supplied is which I²C bus to scan, 0 for Rev 1 boards, 1 for Rev 2 boards, if not supplied the default is 1
-Warning, this new I²C address will still be used after resetting the power on the device
+The busNumber if supplied is which IÂ²C bus to scan, 0 for Rev 1 boards, 1 for Rev 2 boards, if not supplied the default is 1
+Warning, this new IÂ²C address will still be used after resetting the power on the device
     """
     if newAddress < 0x03:
-        print 'Error, I²C addresses below 3 (0x03) are reserved, use an address between 3 (0x03) and 119 (0x77)'
+        print 'Error, IÂ²C addresses below 3 (0x03) are reserved, use an address between 3 (0x03) and 119 (0x77)'
         return
     elif newAddress > 0x77:
-        print 'Error, I²C addresses above 119 (0x77) are reserved, use an address between 3 (0x03) and 119 (0x77)'
+        print 'Error, IÂ²C addresses above 119 (0x77) are reserved, use an address between 3 (0x03) and 119 (0x77)'
         return
     if oldAddress < 0x0:
         found = ScanForPicoBorgReverse(busNumber)
         if len(found) < 1:
-            print 'No PicoBorg Reverse boards found, cannot set a new I²C address!'
+            print 'No PicoBorg Reverse boards found, cannot set a new IÂ²C address!'
             return
         else:
             oldAddress = found[0]
-    print 'Changing I²C address from %02X to %02X (bus #%d)' % (oldAddress, newAddress, busNumber)
+    print 'Changing IÂ²C address from %02X to %02X (bus #%d)' % (oldAddress, newAddress, busNumber)
     bus = smbus.SMBus(busNumber)
     try:
         i2cRecv = bus.read_i2c_block_data(oldAddress, COMMAND_GET_ID, I2C_MAX_LEN)
@@ -173,9 +150,9 @@ Warning, this new I²C address will still be used after resetting the power on th
             foundChip = False
             print 'Missing PicoBorg Reverse at %02X' % (newAddress)
     if foundChip:
-        print 'New I²C address of %02X set successfully' % (newAddress)
+        print 'New IÂ²C address of %02X set successfully' % (newAddress)
     else:
-        print 'Failed to set new I²C address...'
+        print 'Failed to set new IÂ²C address...'
 
 
 # Class used to control PicoBorg Reverse
@@ -183,16 +160,16 @@ class PicoBorgRev:
     """
 This module is designed to communicate with the PicoBorg Reverse
 
-busNumber               I²C bus on which the PicoBorg Reverse is attached (Rev 1 is bus 0, Rev 2 is bus 1)
-bus                     the smbus object used to talk to the I²C bus
-i2cAddress              The I²C address of the PicoBorg Reverse chip to control
+busNumber               IÂ²C bus on which the PicoBorg Reverse is attached (Rev 1 is bus 0, Rev 2 is bus 1)
+bus                     the smbus object used to talk to the IÂ²C bus
+i2cAddress              The IÂ²C address of the PicoBorg Reverse chip to control
 foundChip               True if the PicoBorg Reverse chip can be seen, False otherwise
 printFunction           Function reference to call when printing text, if None "print" is used
     """
 
     # Shared values used by this class
     busNumber               = 1     # Check here for Rev 1 vs Rev 2 and select the correct bus
-    i2cAddress              = 0x44  # I²C address, override for a different address
+    i2cAddress              = 0x44  # IÂ²C address, override for a different address
     bus                     = None
     foundChip               = False
     printFunction           = None
